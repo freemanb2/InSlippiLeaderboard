48277c603db251a82c0b7f954583411c80fdaeab
Brandon Freeman <freemanb2@hotmail.com>
2024-06-06 21:27:32 -0400

    Transfer secrets to github

--- cron/fetchStats.ts
+++ cron/fetchStats.ts
@@ -1,6 +1,6 @@
 import { getPlayerDataThrottled } from './slippi'
 import { GoogleSpreadsheet } from 'google-spreadsheet';
-import creds from '../secrets/creds.json';
+//import creds from '../secrets/creds.json';
 import * as syncFs from 'fs';
 import * as path from 'path';
 import util from 'util';
@@ -9,6 +9,18 @@ import * as settings from '../settings'
 import { exec } from 'child_process';
 const fs = syncFs.promises;
 const execPromise = util.promisify(exec);
+const creds = {
+  "type": "service_account",
+  "project_id": "charged-kiln-189218",
+  "private_key_id": process.env.GOOGLE_PRIVATE_KEY_ID,
+  "private_key": process.env.GOOGLE_PRIVATE_KEY,
+  "client_email": process.env.GOOGLE_CLIENT_EMAIL,
+  "client_id": process.env.GOOGLE_CLIENT_ID,
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/in-slippi-leaderboard%40charged-kiln-189218.iam.gserviceaccount.com"
+}
 
 const getPlayerConnectCodes = async (): Promise<string[]> => {
   const doc = new GoogleSpreadsheet(settings.spreadsheetID);
