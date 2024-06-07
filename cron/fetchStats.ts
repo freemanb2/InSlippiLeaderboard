1e980759cb4f33ee677f934d1f7b3e7813981625
Brandon Freeman <freemanb2@hotmail.com>
2024-06-06 22:35:57 -0400

    Replace newlines

--- cron/fetchStats.ts
+++ cron/fetchStats.ts
@@ -10,12 +10,12 @@ import { exec } from 'child_process';
 
 const fs = syncFs.promises;
 const execPromise = util.promisify(exec);
-// dotenv.config({path: ".env.local"})
+dotenv.config({path: ".env.local"})
 const creds = {
   "type": "service_account",
   "project_id": "charged-kiln-189218",
   "private_key_id": process.env.GOOGLE_PRIVATE_KEY_ID,
-  "private_key": process.env.GOOGLE_PRIVATE_KEY,
+  "private_key": process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
   "client_email": process.env.GOOGLE_CLIENT_EMAIL,
   "client_id": process.env.GOOGLE_CLIENT_ID,
   "auth_uri": "https://accounts.google.com/o/oauth2/auth",
@@ -23,7 +23,6 @@ const creds = {
   "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
   "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/in-slippi-leaderboard%40charged-kiln-189218.iam.gserviceaccount.com"
 }
-console.log(creds["client_id"])
 
 const getPlayerConnectCodes = async (): Promise<string[]> => {
   const doc = new GoogleSpreadsheet(settings.spreadsheetID);
