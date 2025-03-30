51503c37b100a0a8a9021a46e9dbc0da8c18c08b
Brandon Freeman <freemanb2@hotmail.com>
2025-03-30 12:29:56 -0400

    Update query with new schema

--- cron/slippi.ts
+++ cron/slippi.ts
@@ -1,41 +1,46 @@
 import { RateLimiter } from "limiter"
 
 export const getPlayerData = async (connectCode: string) => {
-  const query = `fragment userProfilePage on User {
-    displayName
-    connectCode {
-          code
-          __typename
-        }
-      rankedNetplayProfile {
-            id
-            ratingOrdinal
-            ratingUpdateCount
-            wins
-            losses
-            dailyGlobalPlacement
-            dailyRegionalPlacement
-            continent
-            characters {
-                    id
-                    character
-                    gameCount
-                    __typename
-                  }
-            __typename
-          }
+  const query = `fragment profileFieldsV2 on NetplayProfileV2 {
+    id
+    ratingOrdinal
+    ratingUpdateCount
+    wins
+    losses
+    dailyGlobalPlacement
+    dailyRegionalPlacement
+    continent
+    characters {
+      character
+      gameCount
       __typename
+    }
+    __typename
   }
 
-  query AccountManagementPageQuery($cc: String!) {
-      getConnectCode(code: $cc) {
-            user {
-                    ...userProfilePage
-                    __typename
-                  }
-            __typename
-          }
-  }`;
+  fragment userProfilePage on User {
+    fbUid
+    displayName
+    connectCode {
+      code
+      __typename
+    }
+    rankedNetplayProfile {
+      ...profileFieldsV2
+      __typename
+    }
+    __typename
+  }
+    
+  query AccountManagementPageQuery($cc: String!, $uid: String!) {
+    getConnectCode(code: $cc) {
+      user {
+        ...userProfilePage
+        __typename
+      }
+      __typename
+    }
+  }`
 
   const req = await fetch('https://gql-gateway-2-dot-slippi.uc.r.appspot.com/graphql', {
     headers: {
